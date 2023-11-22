2399c952df197ed088af3f4fa8848ecf7b422eae
jepguzman <69858958+jepguzman@users.noreply.github.com>
2023-11-21 23:03:47 -0600

    Consulta de Películas
    Documentación desde el sitio Oficial de la API
    Consulta las películas en Español

---
+++ movie.js
@@ -0,0 +1,12 @@
+const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2OWYxZjVjOGRlMGQxNWEwYTMzMjlhYjMzNWNmZmZlYyIsInN1YiI6IjY1NWNlZmExZDRmZTA0MDBhYzM5OWZlMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.VAVp4Arhb0h46CfHC45ZmGfAmBU6Mtup7N6MKPZkxkU'
+    }
+  };
+  
+  fetch('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=es-MX&page=1&sort_by=popularity.desc', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
